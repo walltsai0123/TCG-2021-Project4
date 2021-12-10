71b63e45b39fa3ba7caa02e366c6e026cd0b55e7
walltsai0123 <timothy.tsai123@gmail.com>
2021-12-11 01:33:01 +0800

    MCTS ver1.0 70% winrate

--- agent.h
+++ agent.h
@@ -70,10 +70,12 @@ protected:
  * random player for both side
  * put a legal piece randomly
  */
-class player : public random_agent {
+class random_player : public agent {
 public:
-	player(const std::string& args = "") : random_agent("name=random role=unknown " + args),
+	random_player(const std::string& args = "") : agent(args),
 		space(board::size_x * board::size_y), who(board::empty) {
+		if (meta.find("seed") != meta.end())
+			engine.seed(int(meta["seed"]));
 		if (name().find_first_of("[]():; ") != std::string::npos)
 			throw std::invalid_argument("invalid name: " + name());
 		if (role() == "black") who = board::black;
@@ -94,7 +96,156 @@ public:
 		return action();
 	}
 
-private:
+protected:
 	std::vector<action::place> space;
 	board::piece_type who;
+	std::default_random_engine engine;
+};
+class MCTS_player: public random_player{
+	class Node{
+	public:
+		Node(){
+			visit = win = 0;
+			parent = nullptr;
+		};
+		int visit;
+		int win;
+		board b;
+		action::place move;
+		struct Node *parent;
+		std::vector<struct Node*> child;
+	};
+public:
+	MCTS_player(const std::string& args = "")
+		:random_player(args)
+	{
+		sim_counts = 100;
+	}
+	virtual action take_action(const board& state) {
+		return Simulation(state);
+	}
+private:
+	Node *selection(Node *root){
+		if(root->child.empty())	//leaf
+			return root;
+		Node *bestNode = nullptr;
+		float bestValue = -99999.0f;
+		for(Node* current : root->child){
+			if(current->visit == 0)
+				return current;
+			else{
+				float value = (float)current->win / current->visit
+					+ sqrt(2 * log(root->visit) / current->visit);
+				if(value > bestValue){
+					bestNode = current;
+					bestValue = value;
+				}
+			}
+		}
+		return selection(bestNode);
+	}
+	void expansion(Node *node){
+		std::shuffle(space.begin(), space.end(), engine);
+		for (const action::place& move : space) {
+			board after = node->b;
+			if (move.apply(after) == board::legal){
+				Node *newchild = new Node();
+				newchild->parent = node;
+				newchild->move = move;
+				newchild->b = after;
+				node->child.push_back(newchild);
+			}
+		}
+	}
+	bool oneSim(Node *node){
+		random_player *opponent,*myself;
+		if(this->who == board::black){
+			myself = new random_player("name=black  role=black");
+			opponent = new random_player("name=white  role=white");
+		}else if(this->who == board::white){
+			myself = new random_player("name=white  role=white");
+			opponent = new random_player("name=black  role=black");
+		}
+		bool MYTURN = false;
+		board state = node->b;
+		while(true){
+			action move;
+			if(MYTURN){
+				move = myself->take_action(state);
+			}else{
+				move = opponent->take_action(state);
+			}
+			if(move.apply(state) != board::legal)
+				break;
+			MYTURN = !MYTURN;
+		}
+		return !MYTURN;
+	}
+	void backPropagation(Node *current, int win){
+		if(current == nullptr)
+			return;
+		current->visit++;
+		current->win += win;
+		backPropagation(current->parent, win);
+	}
+	action Simulation(const board& state){
+		Node *root = new Node();
+		root->b = state;
+		expansion(root);
+		if(root->child.empty())
+			return action();
+		for(int i = sim_counts; i > 0; --i){
+			Node *current = selection(root);
+			//std::cout<< current->move.position() << std::endl;
+			//std::cout<< current->visit << std::endl;
+			/* if(current->visit > 0){
+				expansion(current);
+				std::cout<< current->child.size() << std::endl;
+				current = current->child[0];
+			} */
+			int win = oneSim(current) ? 1 : 0;
+			backPropagation(current, win);
+			//std::cout<< root->win << "/" << root->visit << std::endl;
+		}
+		float bestWinRate = (float)root->child[0]->win / root->child[0]->visit;
+		action::place bestmove = root->child[0]->move;
+		for(Node *child : root->child){
+			if(child->visit == 0)	continue;
+			float winRate = (float)child->win / child->visit;
+			if(winRate > bestWinRate){
+				bestWinRate = winRate;
+				bestmove = child->move;
+			}
+			//std::cout<< child->win << " " <<  child->visit << std::endl;
+		}
+		//std::cout << root->visit << std::endl;
+		return bestmove;
+	}
+	//variables
+	//std::vector<action::place> space;
+	//board::piece_type who;
+	int sim_counts;
+};
+class player: public random_player{
+public:
+	player(const std::string& args = "")
+		:random_player(args)
+	{
+		if(args.find("search=") != std::string::npos)
+		{
+			std::string search = args.substr(args.find("search=") + 7);
+			search = search.substr(0,search.find(" "));
+			//std::cout<< "search=" << search << std::endl;
+			if(search == "MCTS"){
+				myAgent = new MCTS_player(args);
+			}
+		}
+		else //random
+		{	myAgent = new random_player(args);}
+	}
+	virtual action take_action(const board& state){
+		return myAgent->take_action(state);
+	}
+private:
+	agent *myAgent;
 };
